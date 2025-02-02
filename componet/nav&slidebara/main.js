9e352570bdcb7497456fa25bb55a22302e6578bd
Fouad El-Bakly <foaademad66@gmail.com>
2025-02-02 07:08:00 +0200

    init

--- componet/nav&slidebara/main.js
+++ componet/nav&slidebara/main.js
@@ -158,31 +158,3 @@ window.onload = function () {
 
     
 };
-
-// ==============================================================
-// swiper
-
-
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
